6a9bf8ba1d3464fe0fa5efc2306f1225b54025b3
Sage Gerard <sage@sagegerard.com>
2014-11-19 20:01:15 +0000

    Add Sage Gerard to credits on About page

--- protected/views/info/about.php
+++ protected/views/info/about.php
@@ -1,11 +1,23 @@
-<h1 class="page-title">About Us</h1>
-<img src="/images/adbc_murray.jpg" class="pull-left floating-image">
+<h1>About Us</h1>
 
+<h2>Dr. Meg Murray</h2>
+<img src="/images/adbc_murray.jpg" class="pull-left floating-image">
 <p>Dr. Meg Murray, Professor of Information Systems at Kennesaw State University, has extensive experience, both in academe and in industry, in the area of software design, development and architecture. Her current work has been in the area of web services and using XML as a medium for data exchange and she has been an author and presenter on the technical and societal implications of the software paradigm referred to as <span class="nobr">'Software as a Service.'</span></p> 
 
 <p>She developed a model for a healthcare information systems portal to facilitate the exchange of healthcare data stored in a variety of backend database systems. She has been a recipient of the KSU College of Science and Mathematics Distinguished Teaching Award and has twice been a semifinalist for the Women of the Year in Technology in Georgia.</p>
 
-<h2>Students Involved with ADbC</h2>
+<h2>Sage Gerard</h2>
+<img src="/images/adbc_sage.jpg" class="pull-right floating-image">
+<p>Sage Gerard, a student of Kennesaw State University, has nearly a decade of experience
+in software engineering, web development and related skills. He is responsible
+for the back end and front end design of the 2014 release of ADbC, and has overseen
+the transfer of all content produced since 2007.</p>
+<p>Sage is an honor student that served in the Phi Theta Kappa and Golden Key honor societies.
+He is one of 48 students to be selected nationwide as a NASA Aerospace scholar for his design of a 2018 mission to Mars proposing the use of swarm robotics and a da Vinci Si-inspired automated
+factory. Sage was also nominated for the Barry Goldwater scholarship for his contribution to
+automated experimentation using combinatorial designs. For his outstanding work in technology, he has been named a tech leader by the Mississippi Business Journal alongside Fred Haise and entrepreneurs.</p>
+
+<h2>Legacy ADbC</h2>
 <ul>
     <li>Greg Youree (Student Project Lead 2007-2010)</li>
     <li>Zach Bailes (Student Project Lead 2010-2011)</li>

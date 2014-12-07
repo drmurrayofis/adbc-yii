b612796d75a8d9dfca0f6205659f488460ed223b
Sage Gerard <sage@sagegerard.com>
2014-12-07 03:46:28 +0000

    Update layouts for different use cases, namely for articles and interfaces requiring a sidebar

---
+++ protected/views/layouts/article.php
@@ -0,0 +1,6 @@
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
+    <article class="big-column">
+        <?php echo $content; ?>
+    </article>
+<?php $this->endContent(); ?>

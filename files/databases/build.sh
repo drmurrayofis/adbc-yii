c36b30069b3f462997f4bd9f8ccf3d3bc647bd8a
Sage Gerard <sage@sagegerard.com>
2014-12-08 18:46:58 +0000

    No longer tracking raw SQLite3 databases. Use /files/databases/build.sh to construct databases in bulk

---
+++ files/databases/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Finds all *.sql files under the directory tree
+# where this script is located and runs sqlite3
+# for each script in the same directories.
+#
+# The output databases are named after the .sql
+# files, but have the .sqlite extension.
+#
+main()
+{
+    local here=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+    for i in $(find "$here" -name *.sql); do
+        sqlite3 "${i}ite" < "$i"
+    done
+
+    return 0
+}
+
+main $@
